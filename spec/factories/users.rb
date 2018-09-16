f79eeb9d87d1b055130477817311c29f861e3f39
Scottydu <scottmillion@gmail.com>
2018-09-16 16:28:24 +0700

    First Rspec Tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
-  factory :user do
-    
+#  factory :user do
+#    name 'John'
+#    email 'john.doe@example.com'
+#    password '123456'
+#    active true
+#  end
+
+  factory :random_user, class: User do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password '123456'
+    active true
   end
+
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) } # User.new({})
+  # let(:user) { create(:user) } ... same as User.create({})
+  # build does not save. Create is new + save
+
+  it 'ensures name presence' do
+    # user = User.new(name: 'Joe', email: 'sample@example.com').save
+    user.name = nil
+    expect(user.save).to eq(false)
+  end
+
+  it 'ensures email presence' do
+    user.email = nil
+    expect(user.save).to eq(false)
+  end
+
+  it 'should save successfully' do
+    expect(user.save).to eq(true)
+  end
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
